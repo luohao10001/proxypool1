71c006b1e89b5c7a666e0ad107801cbdf42bae47
luohao10001 <55565872+luohao10001@users.noreply.github.com>
2021-07-22 18:26:09 +0800

    Add files via upload

--- config/source.go
+++ config/source.go
@@ -1,6 +1,6 @@
 package config
 
-import "github.com/Sansui233/proxypool/pkg/tool"
+import "github.com/luohao10001/proxy/pkg/tool"
 
 type Source struct {
 	Type    string       `json:"type" yaml:"type"`
